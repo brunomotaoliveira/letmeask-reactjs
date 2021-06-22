1cec2e098b57f27a095816bbf45348978a0be2a1
brunomotadev <brunomotadev@gmail.com>
2021-06-22 13:03:59 -0300

    Adicionado assets, importado o home

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { Button } from "./components/Button"; //named export
+import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <div>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-    </div>
+    <Home />
   );
 }
 
