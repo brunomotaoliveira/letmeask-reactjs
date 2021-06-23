1b55ed8f34e53d9cd93f61791773ef1665f4fd36
brunomotadev <brunomotadev@gmail.com>
2021-06-23 12:33:10 -0300

    Roteamento e navegação - ReactJS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import { Home } from "./pages/Home";
+import {  BrowserRouter, Route } from 'react-router-dom';
+
+//importação das paginas
 import { NewRoom } from "./pages/NewRoom";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <NewRoom />
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
