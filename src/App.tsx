103a2935d726ca9acdd4536b6fffa856e5b4fb81
brunomotadev <brunomotadev@gmail.com>
2021-06-24 17:20:10 -0300

    hook de autenticação

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,19 @@
-import { createContext, useState, useEffect } from 'react';
 import {  BrowserRouter, Route } from 'react-router-dom';
 
 //importação das paginas
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
-import { auth, firebase } from './services/firebase';
 
-type User = {
-  id: string;
-  name: string;
-  avatar: string;
-}
-
-type AuthContextType = {
-  user: User | undefined;
-  signInWithGoogle: () => Promise<void>;
-}
-
-export const AuthContext = createContext({} as AuthContextType);
+import { AuthContextProvider } from './contexts/AuthContext';
 
 function App() {
-  const [user, setUser] = useState<User>();
-
-  useEffect(() => {
-    auth.onAuthStateChanged(user => {
-      if (user) {
-        const { displayName,  photoURL, uid } = user
-
-              if(!displayName || !photoURL) {
-                throw new Error('Missing information from Google Account.');
-            }
-
-            setUser({
-              id: uid,
-              name: displayName,
-              avatar: photoURL
-            })
-      }
-    })
-  }, [])
-
-  async function signInWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    const result = await auth.signInWithPopup(provider);
-
-        
-            if (result.user) {
-              const { displayName,  photoURL, uid } = result.user
-
-              if(!displayName || !photoURL) {
-                throw new Error('Missing information from Google Account.');
-            }
-
-            setUser({
-              id: uid,
-              name: displayName,
-              avatar: photoURL
-            })
-          }
-  }
-
-
   // {{ }} a primeira chave é para dizer que é um codigo JavaScript, a segunda é para dizer que é um objeto.
   return (
     <BrowserRouter>
-      <AuthContext.Provider value={{ user, signInWithGoogle }}> /
-      <Route path="/rooms/new" component={NewRoom} />
-      <Route path="/" exact component={Home} />
-      </AuthContext.Provider>
+      <AuthContextProvider>
+        <Route path="/rooms/new" component={NewRoom} />
+        <Route path="/" exact component={Home} />
+      </AuthContextProvider>
     </BrowserRouter>
   );
   }
